12af361eb38cecc5b8f6b4c912c126d3140ca812
anadan10102006 <141078706+anadan10102006@users.noreply.github.com>
2023-11-14 19:33:44 -0500

    Update operacion.py

--- operacion.py
+++ operacion.py
@@ -1,5 +1,5 @@
 def caracterbyte():
-    caracter = 'D'
+    caracter = str("Ingrese un caracter")
     valor_ascii = ord(caracter)
     representacion_byte = bin(valor_ascii)[2:]
     print(f"El carácter '{caracter}' en bytes es: {representacion_byte}")
